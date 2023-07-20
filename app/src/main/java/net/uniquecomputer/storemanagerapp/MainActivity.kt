c2a1511c957d0b950161cb9de320397a92af9471
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-20 15:55:37 +0530

    Added Filter and images

--- app/src/main/java/net/uniquecomputer/storemanagerapp/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/storemanagerapp/MainActivity.kt
@@ -2,10 +2,44 @@ package net.uniquecomputer.storemanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import net.uniquecomputer.storemanagerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.filter.setOnClickListener {
+
+            AlertDialog.Builder (this)
+                .setTitle("Filter By")
+                .setSingleChoiceItems(arrayOf("All", "Awaiting Payment", "Received","Processed", "Shipped","Delivered","Cancelled","Returned"), 0) { _, which ->
+                    when (which) {
+                        0 -> {
+
+                        }
+                        1 -> {
+
+                        }
+                        2 -> {
+
+                        }
+                    }
+                  }
+                .setPositiveButton("Ok") { _, _ ->
+                    //do something
+                }
+                .setNegativeButton("Cancel") { _, _ ->
+
+                }
+                .setCancelable(false)
+                .show()
+
+
+        }
+
     }
 }
